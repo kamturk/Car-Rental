306842e33538868af6c81022102f20dc437b89ac
kamturk <turkiewicz.kam@gmail.com>
2023-03-13 20:36:31 +0100

    add reservation data

---
+++ reservation_data.sql
@@ -0,0 +1,86 @@
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (1, 13, 21, 21, '2022-01-01', '2022-01-03');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (2, 2, 22, 21, '2022-04-07', '2022-04-11');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (3, 11, 21, 21, '2022-02-01', '2022-02-03');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (4, 20, 22, 23, '2022-03-07', '2022-03-11');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (5, 13, 25, 25, '2022-04-01', '2022-04-03');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (6, 2, 22, 24, '2022-04-07', '2022-04-11');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (7, 3, 24, 21, '2022-04-01', '2022-04-03');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (8, 15, 22, 21, '2022-05-07', '2022-05-12');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (9, 13, 21, 22, '2022-05-01', '2022-05-03');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (10, 12, 22, 24, '2022-04-07', '2022-04-13');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (11, 13, 21, 23, '2022-04-01', '2022-04-03');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (12, 4, 22, 23, '2022-04-07', '2022-04-10');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (13, 5, 21, 21, '2022-04-01', '2022-04-03');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (14, 7, 22, 22, '2022-04-21', '2022-04-22');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (15, 6, 21, 21, '2022-04-01', '2022-04-03');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (16, 8, 22, 22, '2022-04-22', '2022-04-23');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (17, 13, 23, 23, '2022-04-01', '2022-04-03');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (18, 18, 24, 24, '2022-04-27', '2022-04-30');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (19, 13, 25, 25, '2022-05-01', '2022-05-03');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (20, 2, 25, 21, '2022-05-07', '2022-05-14');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (1, 14, 24, 23, '2022-05-01', '2022-05-03');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (2, 4, 22, 21, '2022-06-07', '2022-06-09');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (3, 13, 21, 21, '2022-06-01', '2022-06-04');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (2, 10, 24, 24, '2022-07-07', '2022-07-11');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (4, 8, 21, 21, '2022-07-11', '2022-07-15');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (5, 11, 22, 21, '2022-07-15', '2022-07-18');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (6, 13, 21, 22, '2022-07-21', '2022-07-23');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (1, 18, 23, 23, '2022-07-07', '2022-07-12');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (8, 16, 24, 24, '2022-08-01', '2022-08-03');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (9, 16, 25, 25, '2022-08-07', '2022-08-09');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (12, 15, 23, 23, '2022-08-22', '2022-08-23');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (20, 6, 22, 21, '2022-08-28', '2022-08-31');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (12, 7, 22, 21, '2022-08-27', '2022-08-31');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (18, 12, 21, 21, '2022-09-01', '2022-09-03');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (20, 10, 21, 22, '2022-09-07', '2022-09-12');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (13, 9, 21, 21, '2022-09-14', '2022-09-18');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (12, 3, 22, 23, '2022-09-16', '2022-09-21');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (1, 1, 21, 22, '2022-10-01', '2022-10-04');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (2, 5, 22, 24, '2022-10-07', '2022-10-10');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (8, 11, 24, 23, '2022-10-18', '2022-10-19');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (9, 9, 25, 21, '2022-10-22', '2022-10-24');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (15, 13, 21, 21, '2022-11-05', '2022-11-08');
+INSERT INTO reservation (customer_id, car_id, pick_up_place_id, drop_off_place_id, pick_up_date, drop_off_date)
+    VALUES (2, 2, 22, 21, '2022-12-07', '2022-12-11');
